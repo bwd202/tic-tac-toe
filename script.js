be9d15d7633219cf33c5cba7347b15cfaf8af499
alex <61472065+bwd202@users.noreply.github.com>
2023-05-24 16:41:23 -0400

    new app

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
 let board = {
-    items: document.querySelectorAll('.board').forEach(item => {
-        item.addEventListener('click', play)
-    }),
-    topRow: [],
-    middleRow: [],
-    bottomRow: [],
-    firstColumn: [],
-    secondColumn: [],
-    thirdColumn: [],
-    topBottomDiagonal: [],
-    bottomTopDiagonal: []
-}
-
-function play(event) {
-    let mark = document.createElement('span')
-    mark.textContent = 'X'
-    mark.classList.add('board-content')
-    if(event.target.hasChildNodes()) return
-    // to prevent adding more Xs
-    event.target.append(mark)
+    square1: document.querySelector
 }
